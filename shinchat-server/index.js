3af7ad0d16a86414665954ce7adac14eb376dfcc
Shreyas Moolya <shreyasmooli007@gmail.com>
2024-02-07 18:12:57 +0530

    Added client env

--- shinchat-server/index.js
+++ shinchat-server/index.js
@@ -21,7 +21,7 @@ const server = app.listen(process.env.PORT,() => {
 
 const io = socket(server,{
     cors:{
-        origin: "http://localhost:3000",
+        origin: (process.env.CLIENT_PORT),
         credentials: true,
     },
 });
